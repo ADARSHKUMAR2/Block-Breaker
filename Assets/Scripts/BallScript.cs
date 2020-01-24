1a03dfac0ea8143b6a3ab3ae16baa90220ae5cd4
adarsh kumar <adarshkumar124@gmail.com>
2020-01-24 17:48:26 +0530

    Second

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField] PaddleScript paddle1;
+    Vector3 paddleToBallVector;
+    bool hasStarted=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector3 paddlePos = new Vector3(paddle1.transform.position.x, paddle1.transform.position.y, -6.4f);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector3 paddlePos = new Vector3(mousePosInUnits, transform.position.y,-6.3f);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
