ec778b6bcb84fbccdaeb67c75da38c50c5a501e7
adarsh kumar <adarshkumar124@gmail.com>
2020-01-27 19:09:49 +0530

    27/01/2020

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
     [SerializeField] PaddleScript paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     Vector3 paddleToBallVector;
     bool hasStarted=false;
     // Start is called before the first frame update
@@ -20,6 +23,15 @@ public class BallScript : MonoBehaviour
         {
             LockBallToPaddle();
             LaunchBallOnClick();
+        }
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
         }
     }
 
